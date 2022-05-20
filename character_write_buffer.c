a26cc1f0f72ed0b412bad344fd43dbe8f0bed27a
iroatu08 <iroatu08@gmail.com>
2022-05-21 00:06:41 +0100

    created all custom functions for printf

--- character_write_buffer.c
+++ character_write_buffer.c
@@ -1,29 +1,42 @@
 #include "main.h"
-/**
- * p_char - writes char to buffer or standard output
- * @inv: the arguments inventory with most commonly used arguments
- * Return: number of chars wrote to buffer
- */
-void p_char(inventory_t *inv)
-{
-	inv->c0 = va_arg(*(inv->args), int);
+#include <unistd.h>
 
-	write_buffer(inv);
-}
 /**
- * p_percent - writes a percent symbol to buffer or stdout
- * @inv: the arguments inventory with most commonly used arguments
- * Return: number of chars wrote to buffer
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ * Description: _putchar uses a local buffer of 1024 to call write
+ * as little as possible
  */
-void p_percent(inventory_t *inv)
+int _putchar(char c)
 {
-	inv->c0 = '%';
+	static char buf[1024];
+	static int i;
 
-	if (inv->space)
+	if (c == -1 || i >= 1024)
 	{
-		inv->space = 0;
-		inv->buffer[--(inv->buf_index)] = '\0';
+		write(1, &buf, i);
+		i = 0;
 	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
 
-	write_buffer(inv);
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
 }
