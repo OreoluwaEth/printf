d4a5c918bb5631f2955b35fc21b832c4de5ba402
Oreoluwa <oreoluwaadepegba@yahoo.com>
2022-05-22 18:26:03 -0700

    betty coding style solved

--- printf_functions.c
+++ printf_functions.c
@@ -131,18 +131,6 @@ int _print_bin(va_list args)
 	}
 	return (count);
 }
-
-/**
- *_printstring - function that print a string
- *@s: pointer to value of each simbol of string
- *Return: counter.
- */
-int _printstring(char *s)
-{
-	int i = 0;
-
-	while (*s != '\0')
-	{
 		_putchar(*s);
 		s++;
 		i++;
